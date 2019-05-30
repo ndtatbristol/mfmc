6e2044c841f34a16cb5ca274dd0225a1d3b23bf6
rltb <rltb@users.noreply.github.com>
2019-05-30 16:21:26 +0100

    Append or create user dataset
    Will append user dataset if dataset_name already present, rather than insist on creating new dataset and failing if dataset name already used. Uses approach taken by mandatory datasets being added, e.g. in fn_MFMC_add_frame by calling fn_hdf5_add_to_dataset

--- Matlab/fn_MFMC_add_user_dataset.m
+++ Matlab/fn_MFMC_add_user_dataset.m
@@ -8,6 +8,11 @@ function fn_MFMC_add_user_dataset(MFMC, ref_or_index_or_loc, dataset_name, datas
 %   dataset_name - name of dataset
 %   dataset_value - value of dataset
 %--------------------------------------------------------------------------
+
+dummy.(dataset_name)=dataset_value;
+
 dataset_path = [fn_hdf5_ref_or_index_or_loc_to_loc(ref_or_index_or_loc, MFMC.fname), '/', dataset_name];
-fn_hdf5_create_dataset(MFMC.fname, dataset_path, dataset_value);
+%fn_hdf5_create_dataset(MFMC.fname, dataset_path, dataset_value);
+fn_hdf5_add_to_dataset(dummy, MFMC.fname, [dataset_path],             'M', [], [size(dataset_value), inf], ndims(dataset_value)+1);
+
 end
