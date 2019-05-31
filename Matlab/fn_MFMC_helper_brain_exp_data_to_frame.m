13aad77b2b5272f0b44deec8615b5a0f09047fe5
rltb <rltb@users.noreply.github.com>
2019-05-31 15:50:31 +0100

    Helper functions for BRAIN exp_data format to MFMC format with example script

---
+++ Matlab/fn_MFMC_helper_brain_exp_data_to_frame.m
@@ -0,0 +1,58 @@
+function [SEQUENCE]=fn_MFMC_helper_brain_exp_data_to_frame(exp_data,MFMC, SEQUENCE,varargin)
+
+    % Convert BRAIN (relevant) exp_data data structure to FRAME of MFMC format
+    % Add to specified SEQUENCE in MFMC file
+
+    % 1st optional input - conversion factor for location data
+    % 2nd optional input - probe orientation vector
+
+    FRAME.MFMC_DATA = exp_data.time_data;
+    
+    conversion_factor=0.0125e-3;
+    
+    if (length(varargin) > 0)
+        conversion_factor=varargin{1};
+        if (length(varargin)>1)
+            probe_orientation_vectors=varargin{2};
+        end
+    end
+        
+    FRAME.PROBE_POSITION = [0; 0; 0];           %PCS origin is at GCS origin
+    if (isfield(exp_data,'location'))
+        FRAME.PROBE_POSITION = [exp_data.location.x*conversion_factor; exp_data.location.y*conversion_factor; exp_data.location.z*conversion_factor]; 
+    end
+    if (exist('probe_orientation_vectors','var'))
+        FRAME.PROBE_X_DIRECTION = probe_orientation_vectors(:,1);
+        FRAME.PROBE_Y_DIRECTION = probe_orientation_vectors(:,2);
+    else
+        FRAME.PROBE_X_DIRECTION = [1; 0; 0];    %PCS x-axis is aligned to GCS x-axis
+        FRAME.PROBE_Y_DIRECTION = [0; 1; 0];    %PCS y-axis is aligned to GCS y-axis
+    end
+    
+    % Get number of current positional vectors in SEQUENCE
+    datasets_to_include = {'PROBE_POSITION'};
+    counter=1;
+    SEQUENCE1 = fn_MFMC_read_sequence_partial(MFMC, SEQUENCE.ref,datasets_to_include);
+    if (isfield(SEQUENCE1,datasets_to_include{1}))
+        counter=size(SEQUENCE1.PROBE_POSITION,3)+1;
+    end
+    
+    FRAME.PROBE_PLACEMENT_INDEX = ones(length(exp_data.tx), 1)*counter; 
+    
+    FRAME.deflate_value=7;
+    
+    %Add the frame to the specified sequence
+    fn_MFMC_add_frame(MFMC, SEQUENCE.ref, FRAME);
+    
+    %Add probe standoff and angle if known
+    if (isfield(exp_data,'location') && isfield(exp_data.location,'standoff'))
+        %Example of adding user dataset to sequence in file once sequence has been added
+        fn_MFMC_add_user_dataset(MFMC, SEQUENCE.location, 'USER_PROBE_STANDOFF', exp_data.location.standoff);
+    end
+    if (isfield(exp_data,'location') && isfield(exp_data.location,'angle1'))
+        %Example of adding user dataset to sequence in file once sequence has been added
+        fn_MFMC_add_user_dataset(MFMC, SEQUENCE.location, 'USER_PROBE_ANGLE', exp_data.location.angle1);
+    end    
+    
+    
+end
---
+++ Matlab/fn_MFMC_helper_brain_exp_data_to_sequence.m
@@ -0,0 +1,43 @@
+function [SEQUENCE]=fn_MFMC_helper_brain_exp_data_to_sequence(exp_data,PROBE,varargin)
+
+    % Convert BRAIN (relevant) exp_data data structure to SEQUENCE of MFMC format
+    % Associate this SEQUENCE with PROBE
+
+    SEQUENCE.TIME_STEP = exp_data.time(2)-exp_data.time(1);
+    SEQUENCE.START_TIME = exp_data.time(1);
+    if (isfield(exp_data,'material_L_velocity'))
+        long_vel=exp_data.material_L_velocity;
+    elseif (~isempty(varargin))
+        long_vel=varargin{1};
+    elseif (isfield(exp_data,'ph_velocity'))
+        long_vel=exp_data.ph_velocity;
+    else
+        disp('ERROR: longnitudinal velocity not specified')
+        return;
+    end
+    if (isfield(exp_data,'material_T_velocity'))
+        shear_vel=exp_data.material_T_velocity;
+    elseif (~isempty(varargin))
+        shear_vel=varargin{2};
+    elseif (isfield(exp_data,'ph_velocity'))
+        shear_vel=exp_data.ph_velocity*0.5;
+    else
+        disp('ERROR: shear velocity not specified')
+        return;
+    end
+    SEQUENCE.SPECIMEN_VELOCITY = [shear_vel, long_vel];
+    
+    SEQUENCE.PROBE_LIST = PROBE.ref; %this is the HDF5 object reference
+
+    for jj = 1:length(exp_data.array.el_xc)
+        SEQUENCE.LAW{jj}.ELEMENT = int32(jj);       %identify individual element
+        SEQUENCE.LAW{jj}.PROBE = PROBE.ref;         %reference to probe
+    end
+    %Now define the focal laws for transmission and reception associated with
+    %each A-scan in data. In Matlab these are defined by indices referring to
+    %the focal laws above; in the file, these are converted into HDF5 object 
+    %references
+    SEQUENCE.transmit_law_index=exp_data.tx;                                                          
+    SEQUENCE.receive_law_index=exp_data.rx;
+
+end
