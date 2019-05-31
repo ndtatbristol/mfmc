f4d61fccfc6176c01403b270e3a34d7c55e7c3b3
rltb <rltb@users.noreply.github.com>
2019-05-31 14:03:48 +0100

    Update fn_hdf5_read_to_matlab.m
    Can now choose to include rather that just exclude specific datasets when reading in from HDF5. If using include list, then only TYPE attribute is read in.

--- Matlab/private/fn_hdf5_read_to_matlab.m
+++ Matlab/private/fn_hdf5_read_to_matlab.m
@@ -4,19 +4,25 @@ function [res, groups] = fn_hdf5_read_to_matlab(fname, location, varargin)
 %   in res. Groups are returned as list, but not examined. Optional
 %   argument lists names of any datasets to exclude
 
+included_only=0;
 if length(varargin) < 1
-    datasets_to_exclude = [];
+    datasets_to_treat_specially = [];
 else
-    datasets_to_exclude = varargin{1};
+    if (length(varargin) < 2 || varargin{2} < 1)
+        datasets_to_treat_specially = varargin{1};
+    else
+        datasets_to_treat_specially = varargin{1};
+        included_only=1;
+    end
 end
 
 a = h5info(fname, location);
 for ii = 1:length(a.Datasets)
     nm = a.Datasets(ii).Name;
-    skip = 0;
-    for jj = 1:length(datasets_to_exclude)
-        if strcmp(nm, datasets_to_exclude{jj})
-            skip = 1;
+    skip = included_only-0;
+    for jj = 1:length(datasets_to_treat_specially)
+        if strcmp(nm, datasets_to_treat_specially{jj})
+            skip = 1-included_only;
             break;
         end
     end
@@ -34,9 +40,24 @@ for ii = 1:length(a.Datasets)
         res.(nm) = h5read(fname, [location, '/', nm]);
     end
 end
-for ii = 1:length(a.Attributes)
-    nm = a.Attributes(ii).Name;
-    res.(nm) = h5readatt(fname, location, nm);
+if (included_only < 1)
+    for ii = 1:length(a.Attributes)
+        nm = a.Attributes(ii).Name;
+        res.(nm) = h5readatt(fname, location, nm);
+    end
+else
+    for ii = 1:length(a.Attributes)
+        nm = a.Attributes(ii).Name;
+        if strcmp(nm, 'TYPE')
+            res.(nm) = h5readatt(fname, location, nm);
+            break;
+        end
+    end
+end
+
+if (~exist('res','var'))
+    % No datasets found. Return empty structure
+    res=[];
 end
 
 groups = a.Groups;
